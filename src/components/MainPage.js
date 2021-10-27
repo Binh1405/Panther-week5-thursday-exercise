3139f9a64756c02f97b3ad3acdc95a746ab240c9
Ngoc Vi Nguyen <ngocvinguyen@192.168.2.6>
2021-10-27 21:25:02 +0700

    see list of 20 news on Main Page

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,5 @@
-import Button from '@restart/ui/esm/Button'
 import React, {useState, useEffect} from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap'
 
 const MainPage = () => {
     const [data, setData] = useState([])
@@ -8,27 +7,33 @@ const MainPage = () => {
     useEffect (() => {
         const getData = async () => {
             const apiKey = process.env.REACT_APP_API_KEY
-            let url = `https://newsapi.org/v2/top-headlines?country=fr&apiKey=${apiKey}`
+            let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
             const res = await fetch (url)
             const data = await res.json()
             setData (data.articles)
+            console.log(data)
         }
         getData()
     }, [])
     
     return (
-        //The user can see a list of 20 news from the top headlines on the Main page.
+        //The user can see a list of 20 news from the top headlines on the Main page!! => only see 19 news..??
+        //The user can see a news title, description, image, date of published, author, & a button which allows me to see more information about the movie. => button not link to a new site
         <div> 
             {data.map((el) => 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: '30rem' }}>
+                <Card.Img variant="top" src={el.urlToImage}/>
                 <Card.Body>
-                <Card.Title>data.title</Card.Title>
+                <Card.Title>{el.title}</Card.Title>
                 <Card.Text>
-                 Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                    {el.description}
+                </Card.Text>   
+                <Card.Text>
+                 {el.content}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Text>Date of publish: {el.publishedAt} </Card.Text>
+                <Card.Text>Author: {el.author} </Card.Text>
+                <Button variant="outline-success" size="sm" href="el.url">Link</Button>
                 </Card.Body>
                 </Card>
             )}
